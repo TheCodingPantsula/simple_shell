023ae52681a8c6900aac746439b9a50b08a2a166
TheCodingPantsula <asmdigital14@gmail.com>
2023-08-25 00:43:08 -0700

    fixed errors

--- getpath.c
+++ getpath.c
@@ -7,7 +7,7 @@
  */
 char *get_command_path(char *command)
 {
-	char *path, *path_cpy, *path_token, *file_path;
+	char *path, *path_cpy, *dir, *file_path;
 	int command_len, dir_len;
 	struct stat buffer;
 
@@ -20,18 +20,18 @@ char *get_command_path(char *command)
 		dir = strtok(path_cpy, ":");
 		while (dir != NULL)
 		{
-			command_len = strlen(cmd);
+			command_len = strlen(command);
 			dir_len = strlen(dir);
-			full_path = malloc(command_len + dir_len + 2);
-			strcpy(full_path, dir);
-			strcat(full_path, "/");
-			strcat(full_path, cmd);
-			if (stat(full_path, &buffer) == 0)
+			file_path = malloc(command_len + dir_len + 2);
+			strcpy(file_path, dir);
+			strcat(file_path, "/");
+			strcat(file_path, cmd);
+			if (stat(file_path, &buffer) == 0)
 			{
 				free(path_cpy);
-				return (full_path);
+				return (file_path);
 			}
-			free(full_path);
+			free(file_path);
 			dir = strtok(NULL, ":");
 		}
 		free(path_cpy);
