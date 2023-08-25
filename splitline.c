0526b6a9453dc12c5222b2771fb55b2edf93803e
TheCodingPantsula <asmdigital14@gmail.com>
2023-08-25 00:09:08 -0700

    splitline function

---
+++ splitline.c
@@ -0,0 +1,37 @@
+#include "shell.h"
+
+#define MAX_ARGS 64
+
+/**
+ * split_line - split a command line itno arguments
+ * @line: command line to split
+ * Return: Array of arguments
+ */
+char **split_line(char *line)
+{
+	char **args = malloc(MAX_ARGS * sizeof(char *));
+	int arg_count = 0;
+	int i, r;
+
+	for (i = 0; line[i] != '\0'; i++)
+	{
+		while (line[i] == ' ')
+		{
+			i++;
+		}
+		if (line[i] == '\0')
+		{
+			break;
+		}
+		for (r = i; line[r] != '\0' && line[r] != ' '; r++)
+		{}
+		args[arg_count] = malloc(r - i + 1);
+		strncpy(args[arg_count], &line[i], r - i);
+		args[arg_count][r - i] = '\0';
+		arg_count++;
+		i = r;
+	}
+
+	args[arg_count] = NULL;
+	return (args);
+}
